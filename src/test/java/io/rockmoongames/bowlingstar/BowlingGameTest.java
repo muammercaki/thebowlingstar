1e2a1b73ba7130a57d26c95dd1732e3927ccf292
Muammer <caki451@gmail.com>
2020-11-14 22:40:34 +0300

    Add first test

--- src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
@@ -1,11 +1,20 @@
 package io.rockmoongames.bowlingstar;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BowlingGameTest {
-
+    private static WebDriver driver;
+    private static String baseUrl="https://testautomationu.applitools.com/git-tutorial/chapter3.2.html";
     @Test
     public void jessicaTestOne() {
+        driver=new ChromeDriver();
+        driver.get(baseUrl);
+    }
 
+    @Test
+    public void tearDown(){
+        driver.quit();
     }
 }
